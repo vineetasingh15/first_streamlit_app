ffbdc6601fc1405715fa8a1296dae7f30e3a2a7b
vineetasingh15 <130393838+vineetasingh15@users.noreply.github.com>
2023-04-15 20:39:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.stop()
 
 streamlit.header("The fruit list contains:")
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
          return my_cur.fetchall()
 
